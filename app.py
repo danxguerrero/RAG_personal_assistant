c323864589b1af104923fbaeac16fafec5f8373b
danxguerrero <danxguerrero@yahoo.com>
2025-08-31 18:25:46 -0400

    Implement chunking of uploaded txt files

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 api_key = st.secrets["GOOGLE_API_KEY"]
 
+
+# Upload txt files and shows a preview if loaded successfully
 uploaded_file = st.file_uploader("Upload a .txt file", type=["txt"])
 
 if uploaded_file:
@@ -9,4 +12,12 @@ if uploaded_file:
     st.write("File uploaded successfully! Preview:")
     st.write(f"{text[:500]}...")
 
-    
+# Chunk the txt file contents using langchain
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = text_splitter.split_text(text)
+st.write(f"Text successfully chunked: {chunks}")
+
